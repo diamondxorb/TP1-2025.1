b033a185762e7b492865cd4598d9efab28751a73
Júlia <168246513+diamondxorb@users.noreply.github.com>
2025-04-10 10:45:45 -0300

    Create Consumo de energia elétrica.java

--- Consumo de energia elétrica.java
+++ Consumo de energia elétrica.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
+
         double consumo, custoConsumo, dinheiro;
-        
+
         consumo = Double.parseDouble(scanner.nextLine());
-        
+
         if(consumo <= 500) {
             dinheiro = (consumo*0.02)+5;
         } else if(consumo <= 1000) {
@@ -16,9 +16,10 @@ public class Main {
         } else {
             dinheiro = 355+((consumo-1000)*0.1);
         }
-        
+
         custoConsumo = dinheiro-5;
-        
-        System.out.printf("%.2f 5.00 %.2f", custoConsumo, dinheiro);
+
+        //O Locale.US antes das aspas no printf corrige a vírgula automática em números decimais//
+        System.out.printf(Locale.US,"%.2f 5.00 %.2f", custoConsumo, dinheiro);
     }
 }
